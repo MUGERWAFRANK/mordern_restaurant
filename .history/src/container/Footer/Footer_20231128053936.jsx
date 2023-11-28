78cdd32b98fb361fc846deaa55384166941b88c9
mugerwa frank <fsimple47@gmail.com>
2023-11-28 05:43:43 +0300

    changes

--- .history/src/container/Footer/Footer_20231128053936.jsx
+++ .history/src/container/Footer/Footer_20231128053936.jsx
@@ -14,9 +14,9 @@ const Footer = () => (
     <div className="app__footer-links">
       <div className="app__footer-links_contact">
         <h1 className="app__footer-headtext">Contact Us</h1>
-        <p className="p__opensans">9 CITY SQUARE, KAMPALA, KAMPALA 10019, UGANDA</p>
+        <p className="p__opensans">9 W 53rd St, New York, NY 10019, USA</p>
         <p className="p__opensans">+256 778-880-079</p>
-        <p className="p__opensans">+256 705-718-118</p>
+        <p className="p__opensans">+1 212-555-1230</p>
       </div>
 
       <div className="app__footer-links_logo">
@@ -40,7 +40,7 @@ const Footer = () => (
     </div>
 
     <div className="footer__copyright">
-      <p className="p__opensans">2023 Gericht. All Rights reserved.</p>
+      <p className="p__opensans">2021 Gericht. All Rights reserved.</p>
     </div>
 
   </div>
